72f682171b66cf17345bb94be6ebbd6b795938a2
Abhishek jadhav <109812422+Abhishekjadhavv@users.noreply.github.com>
2022-07-24 17:11:00 +0530

    Add files via upload

---
+++ charascii.c
@@ -0,0 +1,11 @@
+// 6. Write a program which takes a character as an input and displays its ASCII code.
+
+#include<stdio.h>
+
+int main(){
+    char ch;
+    printf("Enter a character\n");
+    scanf("%c",&ch);
+    printf("its ASCII code is %d",ch);
+    return 0;
+}
